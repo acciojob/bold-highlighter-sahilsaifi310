30375f22a208872a67b7a46325aaeed073a85848
sahilsaifi310 <shahilsaifi750@gmail.com>
2025-01-02 14:37:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function highlight() {
-    //Write your code here
+     let boldWords = document.querySelectorAll('strong');
+    
+   
+    boldWords.forEach(function(word) {
+        word.style.color = 'green';
+    });
 
 
 }
 
 
 function return_normal() {
-    //Write your code here
+      let boldWords = document.querySelectorAll('strong');
+    
+   
+    boldWords.forEach(function(word) {
+        word.style.color = 'black';
+    });
 
     
 }
